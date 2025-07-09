bb9be854b149f0e3eb2a28e676e71ff59c2621f6
WantedFelconer <97464081+WantedFelconer@users.noreply.github.com>
2025-07-09 12:09:29 +0600

    Add files via upload

--- HomeTask02.java
+++ HomeTask02.java
@@ -1,7 +1,24 @@
-//HomeTask02
 
-public class HomeTask02{
-    public static void main(String[] args){
-        
+//HomeTask02
+import java.util.Scanner;
+
+public class HomeTask02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        for (int i = 0; i < text.length(); i++) {
+            if ((int) text.charAt(i) == (int) 'a') {
+                System.out.print((char) 122);
+                continue;
+            }
+
+            if (((int) text.charAt(i) >= (int) 'a') && ((int) text.charAt(i) <= (int) 'z')) {
+                char cur = text.charAt(i);
+                int prev = (int) cur - 1;
+                char word = (char) prev;
+                System.out.print(word);
+            }
+        }
+        sc.close();
     }
 }
